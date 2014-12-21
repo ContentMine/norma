cc90f2d06404a7f825c39253d2309b62098bccf9
petermr <petermr@users.noreply.github.com>
2014-12-21 14:38:20 +0000

    first commit - dummy code

---
+++ src/test/java/org/xmlcml/nhtml/DummyTest.java
@@ -0,0 +1,13 @@
+package org.xmlcml.nhtml;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void testDummy() {
+		Assert.assertTrue(true);
+	}
+}
