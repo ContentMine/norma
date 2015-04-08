14341a1ba80fde6d12a75de25e0af41a72f53ade
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2015-04-08 14:06:15 +0100

    added PDF conversion

---
+++ src/main/java/org/xmlcml/norma/input/pdf/PDF2TXTConverter.java
@@ -0,0 +1,26 @@
+package org.xmlcml.norma.input.pdf;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+public class PDF2TXTConverter {
+
+	public PDF2TXTConverter() {
+		
+	}
+	
+	public String readPDF(InputStream filesInputStream, boolean sortByPosition) throws IOException {
+		PDDocument doc=PDDocument.load(filesInputStream);
+		PDFTextStripper pdfStripper=new PDFTextStripper();
+		pdfStripper.setSortByPosition(sortByPosition);
+		String s = pdfStripper.getText(doc);
+//		char[] origText=pdfStripper.getText(doc).toCharArray();
+		doc.close();
+		return s;
+//		return new String(origText);
+	}
+		 
+}
