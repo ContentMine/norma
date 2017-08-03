c0f4ea4bdc225064e10a4a22ebdb3a99214358cf
J Brook <jkbcm@users.noreply.github.com>
2017-08-03 17:02:28 +0100

    Add assertions of table rowcount after subtable restructure

--- src/test/java/org/xmlcml/norma/table/SubtableTest.java
+++ src/test/java/org/xmlcml/norma/table/SubtableTest.java
@@ -7,10 +7,12 @@ package org.xmlcml.norma.table;
 
 import java.io.File;
 import java.io.IOException;
+import nu.xom.Elements;
 import org.junit.Test;
 import org.xmlcml.html.HtmlElement;
 import org.xmlcml.norma.NormaFixtures;
 import org.xmlcml.xml.XMLUtil;
+import org.junit.Assert;
 
 /**
  *
@@ -31,7 +33,12 @@ public class SubtableTest {
 		HtmlElement htmlElement = converter.convert();
 		File file = new File(NormaFixtures.TARGET_DIR, "table/subtables/10.1007_s00213-015-4198-1.svg.html");
 		XMLUtil.debug(htmlElement, file, 1);
-                // TODO Assert before and after row counts
+
+                int restructuredTableRowCount = getRestructuredRowCount(htmlElement);
+                
+                // Restructured table has children: 
+                // caption, header row and 3 subtables, no top-level observation rows
+                Assert.assertEquals(5, restructuredTableRowCount);
 	}
         
         @Test
@@ -43,7 +50,12 @@ public class SubtableTest {
 		HtmlElement htmlElement = converter.convert();
 		File file = new File(NormaFixtures.TARGET_DIR, "table/subtables/10.1136.bmjopen-2016-12335_table5.html");
 		XMLUtil.debug(htmlElement, file, 1);
-                // TODO Assert before and after row counts
+
+                int restructuredTableRowCount = getRestructuredRowCount(htmlElement);
+                
+                // Restructured table has children: 
+                // caption, header row and 2 subtables, no top-level observation rows
+                Assert.assertEquals(4, restructuredTableRowCount);
 	}
         
         @Test
@@ -55,7 +67,14 @@ public class SubtableTest {
 		HtmlElement htmlElement = converter.convert();
 		File file = new File(NormaFixtures.TARGET_DIR, "table/subtables/10.1016.j.pec.2005.10.002_table3.html");
 		XMLUtil.debug(htmlElement, file, 1);
-                // TODO Assert before and after row counts
+
+                int restructuredTableRowCount = getRestructuredRowCount(htmlElement);
+                
+                // Restructured table has children: 
+                // caption, header row and 2 subtables, no top-level observation rows
+                // FIXME currently fails to be fully correctly cell-resolved due to superscripts
+                // pushing cells onto the next row in the wrong column
+                Assert.assertEquals(4, restructuredTableRowCount);
 	}
         
         @Test
@@ -67,7 +86,12 @@ public class SubtableTest {
 		HtmlElement htmlElement = converter.convert();
 		File file = new File(NormaFixtures.TARGET_DIR, "table/subtables/10.1136.bmjopen-2016-12335_table6.html");
 		XMLUtil.debug(htmlElement, file, 1);
-                // TODO Assert before and after row counts
+
+                int restructuredTableRowCount = getRestructuredRowCount(htmlElement);
+                
+                // Restructured table has children: 
+                // caption, header row and 8 content rows
+                Assert.assertEquals(10, restructuredTableRowCount);
 	}
         
         @Test
@@ -79,7 +103,12 @@ public class SubtableTest {
 		HtmlElement htmlElement = converter.convert();
 		File file = new File(NormaFixtures.TARGET_DIR, "table/subtables/10.1080.15504263.2015.1113842_table3.html");
 		XMLUtil.debug(htmlElement, file, 1);
-                // TODO Assert before and after row counts
+                
+                int restructuredTableRowCount = getRestructuredRowCount(htmlElement);
+                
+                // Restructured table has children: 
+                // caption, header row and 4 content rows
+                Assert.assertEquals(6, restructuredTableRowCount); 
 	}
         
         /// 10.1186.1471-2407-12-560_table2.svg
@@ -92,6 +121,30 @@ public class SubtableTest {
 		HtmlElement htmlElement = converter.convert();
 		File file = new File(NormaFixtures.TARGET_DIR, "table/subtables/10.1186.1471-2407-12-560_table2.html");
 		XMLUtil.debug(htmlElement, file, 1);
-                // TODO Assert before and after row counts
-	}
+                
+                int restructuredTableRowCount = getRestructuredRowCount(htmlElement);
+                
+                // Restructured table has children: 
+                // caption, header row and 6 content rows, including a subtable
+                Assert.assertEquals(8, restructuredTableRowCount); 
+        }
+        
+        /**
+         * Helper method
+         * 
+         * @return The number of children of the main (outermost) table element
+         */
+        private int getRestructuredRowCount(HtmlElement htmlElement) {
+                int restructuredTableRowCount = 0;
+                
+                HtmlElement body = (HtmlElement) htmlElement.getChild(0);
+                if (body != null) {
+                    HtmlElement table = (HtmlElement) body.getChild(0);
+                    if (table != null) {
+                        restructuredTableRowCount = table.getChildCount();
+                    }
+                }   
+                
+                return restructuredTableRowCount;
+        }
 }
