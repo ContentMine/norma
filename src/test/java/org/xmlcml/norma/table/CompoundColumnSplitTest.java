3ac8dbd569e4872cb14b29a5facf23de1bbf08ba
J Brook <jkbcm@users.noreply.github.com>
2017-11-09 20:13:30 +0000

    Added tests

--- src/test/java/org/xmlcml/norma/table/CompoundColumnSplitTest.java
+++ src/test/java/org/xmlcml/norma/table/CompoundColumnSplitTest.java
@@ -18,14 +18,14 @@ import org.xmlcml.xml.XMLUtil;
  * @author jkb
  */
 public class CompoundColumnSplitTest {
-        @Test
-        // Table without any subtables just simple top-level rows -- regression test
-        public void testSplitColumnsWithIntegersAndFloats1() throws IOException {
-					File inputFile = new File(NormaFixtures.TEST_TABLE_DIR, "compoundcolumns/10.1186.1471-2407-12-560_table1.svg");
-					SVGTable2HTMLConverter converter = new SVGTable2HTMLConverter();
-					converter.readInput(inputFile);
-					HtmlElement htmlElement = converter.convert();
-					File file = new File(NormaFixtures.TARGET_DIR, "table/compoundcolumns/10.1186.1471-2407-12-560_table1.html");
-					XMLUtil.debug(htmlElement, file, 1);
-				}
+    @Test
+    // Table without any subtables just simple top-level rows -- regression test
+    public void testSplitColumnsWithIntegersAndFloats1() throws IOException {
+        File inputFile = new File(NormaFixtures.TEST_TABLE_DIR, "compoundcolumns/10.1186.1471-2407-12-560_table1.svg");
+        SVGTable2HTMLConverter converter = new SVGTable2HTMLConverter();
+        converter.readInput(inputFile);
+        HtmlElement htmlElement = converter.convert();
+        File file = new File(NormaFixtures.TARGET_DIR, "table/compoundcolumns/10.1186.1471-2407-12-560_table1.html");
+        XMLUtil.debug(htmlElement, file, 1);
+    }
 }
